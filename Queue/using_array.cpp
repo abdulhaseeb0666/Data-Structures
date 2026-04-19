1f14006066fe9561affad2c7e6b28d634cef9100
abdulhaseeb0666 <abdulhaseebah7886786@gmail.com>
2026-04-19 06:55:06 +0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	new file:   Queue/using_array.cpp

---
+++ Queue/using_array.cpp
@@ -0,0 +1,160 @@
+#include<iostream>
+using namespace std;
+
+class Queue{
+    private:
+        int arr[5];
+        int front = -1;
+        int rear = -1;
+
+    public:
+        Queue(){
+            for(int i=0 ; i<5; i++){
+                arr[i]=0;
+            }
+        }
+
+        bool isEmpty(){
+            if(front == -1 && rear == -1){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        bool isFull(){
+            if(rear == sizeof(arr)-1){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+
+        void enque(int val){
+            if(isFull()){
+                cout<<"Queue Overflowed...";
+                return;
+            }
+
+            else if(isEmpty()){
+                rear ++;
+                front ++;
+            }
+            else{
+                rear ++;
+            }
+            
+            arr[rear] = val;
+        }
+
+        int denque(){
+            if(isEmpty()){
+                cout<<"Queue Underflowed...";
+                return 0;
+            }
+
+            if(front == rear){
+                int popednumber = arr[front];
+                arr[front] == 0;       
+                front = -1;
+                rear = -1;
+            }
+            
+            else {
+                int popednumber = arr[front];
+                arr[front] == 0;       
+                front ++;
+            }
+
+        }
+
+        int count(){
+            int number = 0;
+            for(int i=front ; i<=rear ; i++){
+                number ++;
+            }
+            return number;
+        }
+
+        void display(){
+            for(int i=0; i<5 ; i++){
+                cout<<arr[i]<<"\t";
+            }
+        }
+
+
+};
+
+int main(){
+    Queue q1;
+    int option , popednunmber;
+    
+    do{
+        std :: cout<<"\n\nChose an option from the menu below:\n";
+        std :: cout<<"1. isEmpty"<<endl;
+        std :: cout<<"2. isFull"<<endl;
+        std :: cout<<"3. Push"<<endl;
+        std :: cout<<"4. Pop"<<endl;
+        std :: cout<<"5. Change"<<endl;
+        std :: cout<<"6. Count"<<endl;
+        std :: cout<<"7. Display"<<endl;
+
+        cout<<"Enter your option(0 to exit): "; 
+        cin>>option;
+
+        switch(option){
+            
+            case 1:
+                {if(q1.isEmpty()){
+                    std :: cout<<"Queue is Empty";
+                }
+                else{
+                    std :: cout<<"Queue is not Empty";
+                }
+                break;}
+                
+            case 2:
+                {if(q1.isFull()){
+                    std :: cout<<"Queue is Full";
+                }
+                else{
+                    std :: cout<<"Queue is not Full";
+                }
+                break;}
+            
+            case 3:
+                {int n;
+                std :: cout<<"Enter an integer to add to queue: ";
+                cin>>n;
+                q1.enque(n);
+                break;}
+            
+            case 4:
+                {popednunmber = q1.denque();
+                std :: cout<<"Integer poped from stack is: "<<popednunmber<<endl;
+                break;}
+            
+            
+            case 5:
+                {std :: cout<<"The total integers in Queue are: "<<q1.count()<<endl;
+                break;} 
+
+            case 6:
+                {std :: cout<<"All integers in Queue are given below: "<<endl;
+                q1.display();
+                break;}
+
+            default:
+                {std :: cout<<"Please enter a valid choice"<<endl;
+                break;}
+        }
+
+
+    }while (option != 0);
+    
+    
+
+}
