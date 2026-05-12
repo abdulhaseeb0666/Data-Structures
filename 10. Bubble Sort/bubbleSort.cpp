cd6b0d1da93bf62ea61df83cc34d6a100328ecde
abdulhaseeb0666 <abdulhaseebah7886786@gmail.com>
2026-05-12 13:39:10 +0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	new file:   10. Bubble Sort/bubbleSort.cpp
    	modified:   9. Insertion Sort/insertionSort.cpp

--- 10. Bubble Sort/bubbleSort.cpp
+++ 10. Bubble Sort/bubbleSort.cpp
@@ -14,19 +14,15 @@ int main(){
         cout<<arr[i]<<"\t";
     }
 
-    // Insertion Sort Algorithm
-    int i = 0;
-    int key = 0;
-    for(int i=1 ; i<5 ; i++){
-        key = arr[i];
-        int j = i-1;
-
-        while(j>=0 && arr[j]>key){
-            arr[j+1] = arr[j];
-            j = j-1;    
+    // Bubble Sort Algorithm
+    for(int i=0 ; i<5 ; i++){
+        for(int j=0 ; j<5-1 ; j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
         }
-
-        arr[j+1] = key;
     }
 
     cout<<"\nSorted Array: \n";
